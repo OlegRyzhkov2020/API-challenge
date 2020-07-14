dcef9edead6b8ae0adaf3fe1f50aa3493fdc4114
OlegRyzhkov2020 <61919810+OlegRyzhkov2020@users.noreply.github.com>
2020-07-14 14:28:52 -0500

    WeatherPy case: hemisphere analysis

--- VacationPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++ VacationPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -2,4 +2,4 @@
 weather_api_key = "YOUR KEY HERE!"
 
 # Google API Key
-g_key = "AIzaSyAHBrO9P-qzvqmadHsuzEKRskbtE0RTeZE"
+g_key = "YOUR KEY HERE!"
--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -2,4 +2,4 @@
 weather_api_key = "c6f6a86e8a097acc28080af8686cb857"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAHBrO9P-qzvqmadHsuzEKRskbtE0RTeZE"
